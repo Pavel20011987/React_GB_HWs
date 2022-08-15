9fb7f73ba670aa2c91ec4d62b6b0c514bdba5c7d
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-08-16 00:12:11 +0300

    Commit HW_3

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,37 +1,111 @@
-//import logo from './logo.svg';
-//import './App.css';
-//import Message from "./Message";
+import { useState, useEffect, useCallback, useRef } from "react";
+import MessagesList from "./components/messagesList/MessagesList";
+import Tasks from "./components/tasks/Tasks";
+import "./App.css";
+import MessageForm from "./components/messageForm/MessageForm";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ChatsList from "./components/chatsList/ChatsList";
+import { TextField } from "@mui/material";
 
-//function App() {
-//   const name = 'Pavel';
-//   return ( 
-//<div>
-//        <Message name = { name }/>  
-// </div>
-//    );
-//}
-
-//export default App;
-import React from 'react';
-import './App.css';
-//import { Fruit } from './components/Fruit';
-//import { Message } from './components/Message';
-import { RenderPage } from './components/RenderPage';
+const botMessage = { author: "Bot", body: "" };
+const theme = createTheme({
+  spacing: [0, 4, 8, 16],
+  components: {
+    MuiButton: {
+      defaultProps: {
+        size: "big",
+        variant: "contained",
+        margin: "dense",
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        variant: "filled",
+        size: "small",
+      },
+    },
+    MuiStack: {
+      defaultProps: {
+        spacing: 1,
+      },
+    },
+  },
+});
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("John");
+  const [messageList, setMessageList] = useState([]);
+  const [chatsList] = useState([
+    {
+      id: 1,
+      header: "some header",
+      messages: [],
+    },
+    {
+      id: 2,
+      header: "some header2",
+      messages: [],
+    },
+    {
+      id: 3,
+      header: "some header3",
+      messages: [],
+    },
+  ]);
+  const [idCounter, setIdCounter] = useState(0);
+  const formRef = useRef(null);
 
-    return (
+  const getNewId = useCallback(() => {
+    const newId = idCounter + 1;
+    setIdCounter(newId);
+    return newId;
+  }, [idCounter]);
 
-        <div className = "App">
-            <header className="App-header" >
-                { /* My First React App */}
+  const addMessage = useCallback(
+    (newMessage) => {
+      setMessageList([...messageList, { ...newMessage, id: getNewId() }]);
+    },
+    [getNewId, messageList]
+  );
 
-                { /* <Message name="Pavel" status="done!"></Message> */} { /* <Fruit></Fruit> */}
-                <RenderPage />
+  const botSendMessage = useCallback(
+    (messages) => {
+      if (messages.length === 0) return;
+      const lastMessage = messages.at(-1);
+      if (lastMessage.author === botMessage.author) return;
+      addMessage({ ...botMessage, body: `${lastMessage.author} write in chat` });
+      formRef.current.focus();
+    },
+    [addMessage]
+  );
 
-            </header>
+  useEffect(() => {
+    setTimeout(() => botSendMessage(messageList), 1500);
+  }, [messageList, botSendMessage]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Tasks />
+
+        <div className="chat">
+          <div className="side">
+            <div className="chat-user">
+              <TextField type="text" label="Current User" value={currentUser} onChange={(e) => setCurrentUser(e.target.value)} />
             </div>
-    );
+
+            <ChatsList chats={chatsList} />
+          </div>
+
+          <div className="chat-group">
+            <MessagesList messages={messageList} currentUser={currentUser} />
+
+            <MessageForm onMessageSend={addMessage} ref={formRef} />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- my_app/src/components/MessagesList.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function MessagesList(props) {
-
-    return props.messages.map((message) => 
-    <div className="message">{'Сообщение от '+ message.autor + ': ' + message.text}</div>);
-}
-
