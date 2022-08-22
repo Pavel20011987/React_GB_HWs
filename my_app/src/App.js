bd2f3395f0a3ca2525afb96137b3daf0bde428d0
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-08-23 00:52:23 +0300

    HW_5 done

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -6,45 +6,62 @@ import Chat from "./pages/Chat";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import Counter from "./pages/Counter";
 
 const theme = createTheme({
-  spacing: [0, 4, 8, 16],
-  components: {
-    MuiButton: {
-      defaultProps: {
-        size: "big",
-        variant: "contained",
-        margin: "dense",
-      },
+    spacing: [0, 4, 8, 16],
+    components: {
+        MuiButton: {
+            defaultProps: {
+                size: "big",
+                variant: "contained",
+                margin: "dense",
+            },
+        },
+        MuiTextField: {
+            defaultProps: {
+                variant: "filled",
+                size: "small",
+            },
+        },
+        MuiStack: {
+            defaultProps: {
+                spacing: 1,
+            },
+        },
     },
-    MuiTextField: {
-      defaultProps: {
-        variant: "filled",
-        size: "small",
-      },
-    },
-    MuiStack: {
-      defaultProps: {
-        spacing: 1,
-      },
-    },
-  },
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path={"/"} element={<SiteLayout />}>
-          <Route index element={<Site />} />
-          <Route path={"/profile"} element={<Profile />} />
-        </Route>
-        <Route path={"/chat"} element={<Chat />} />
-        <Route path={"/chat/:chatId"} element={<Chat />} />
-        <Route path={"*"} element={<NotFound />} />
-      </Routes>
-    </ThemeProvider>
-  );
+    return ( <
+        ThemeProvider theme = { theme } >
+        <
+        Routes >
+        <
+        Route path = { "/" }
+        element = { < SiteLayout / > } >
+        <
+        Route index element = { < Site / > }
+        /> <
+        Route path = { "/profile" }
+        element = { < Profile / > }
+        /> <
+        Route path = { "/counter" }
+        element = { < Counter / > }
+        /> < /
+        Route > <
+        Route path = { "/chat" }
+        element = { < Chat / > }
+        /> <
+        Route path = { "/chat/:chatId" }
+        element = { < Chat / > }
+        /> <
+        Route path = { "*" }
+        element = { < NotFound / > }
+        /> < /
+        Routes > <
+        /ThemeProvider>
+    );
 }
 
 export default App;
