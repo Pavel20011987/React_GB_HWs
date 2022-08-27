97b8192a372d53fc8fb00da9d12cbb5e00e1ca81
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-08-27 20:26:51 +0300

    HW_6 done

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -35,32 +35,16 @@ const theme = createTheme({
 function App() {
     return ( <
         ThemeProvider theme = { theme } >
-        <
-        Routes >
-        <
-        Route path = { "/" }
-        element = { < SiteLayout / > } >
-        <
-        Route index element = { < Site / > }
-        /> <
-        Route path = { "/profile" }
-        element = { < Profile / > }
-        /> <
-        Route path = { "/counter" }
-        element = { < Counter / > }
-        /> < /
-        Route > <
-        Route path = { "/chat" }
-        element = { < Chat / > }
-        /> <
-        Route path = { "/chat/:chatId" }
-        element = { < Chat / > }
-        /> <
-        Route path = { "*" }
-        element = { < NotFound / > }
-        /> < /
-        Routes > <
-        /ThemeProvider>
+        <Routes>
+        <Route path = { "/" } element = { <SiteLayout/> } >
+        <Route index element = { < Site/> }/>
+          <Route path = { "/profile" } element = { < Profile/> }/>
+          <Route path = { "/counter" } element = { < Counter/> }/>
+        </Route>
+        <Route path = { "/chat" } element = { <Chat/> }/>
+          <Route path = { "*" } element = { <NotFound/> }/>
+      </Routes>
+          </ThemeProvider>
     );
 }
 
